a13621e81479260abc68a08cfc4a1e310844e8cf
Gabriel Morais <gmorais1994@gmail.com>
2017-07-31 12:57:11 -0300

    v01/08

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,22 +14,62 @@ angular.module('starter.controllers', [])
        };
 })
 
-.controller('HomeCtrl', function($scope, $http, $state, $timeout) {
+.controller('HomeCtrl', function($scope, $http, $state, $timeout, Sessao, $stateParams, SenseService) {
+
+$scope.senses = SenseService.todos();
+
+  var usuario = Sessao.obter();
     $http.get('http://104.131.166.166:3000/home').then(function(resposta){
     $scope.items = resposta.data;
  })
 
-  $scope.backButton = function(){
-    $state.go("tab.profile");
-  }
+ $scope.senseInsert = function(item, sense){
+   var usuario = Sessao.obter();
+   $scope.senseInserted = {}
+   $scope.senseInserted.artName = item;
+   $scope.senseInserted.senseColor = sense;
+   $scope.senseInserted.username = usuario.username;
 
+   $http.put('http://104.131.166.166:3000/item/'+$scope.senseInserted.artName+'/sense', $scope.senseInserted).then(function(response){
+     if(response.senseInserted){
+       console.log('Put executado com sucesso');
+     }
+     console.log('erro');
+   });
+ }
 
-  // var bgColorArray = ['#FF94A1','#FFF589','#BEC4F2'],
-  //   selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
-  //
-  //   [].forEach.call(document.getElementsByTagName('itemHomeInfo'), function(el) {
-  //     el.style.backgroundColor = selectBG;
-  // });
+ $scope.collabInsert = function(item, collabText){
+   var usuario = Sessao.obter();
+   $scope.collabInserted = {}
+   $scope.collabInserted.artName = item;
+   $scope.collabInserted.collabText = collabText;
+   $scope.collabInserted.username = usuario.username;
+
+   $http.put('http://104.131.166.166:3000/item/'+$scope.collabInserted.artName+'/collab', $scope.collabInserted).then(function(response){
+     if(response.collabInserted){
+       console.log('Put executado com sucesso');
+     }
+     console.log('erro');
+   });
+ }
+
+ $scope.viewInsert = function(item){
+   var usuario = Sessao.obter();
+   $scope.viewInserted = {}
+   $scope.viewInserted.artName = item.artName;
+   $scope.viewInserted.username = $scope.usuario.username;
+
+   $http.put('http://104.131.166.166:3000/'+$scope.viewInserted.artName+'/view', $scope.viewInserted).then(function(response){
+     if(response.viewInserted){
+       console.log('Put executado com sucesso');
+     }
+     console.log('erro');
+   });
+ }
+
+  $scope.goItem = function(item){
+    $state.go('tab.item', {artName: item.artName});
+}
 
   $scope.doRefresh = function() {
     $timeout( function() {
@@ -57,7 +97,9 @@ angular.module('starter.controllers', [])
      });
    }
  };
-
+ $scope.goItem = function(item){
+   $state.go('tab.item', {artName: item.artName});
+ }
  $scope.doRefresh = function() {
    $timeout( function() {
      $scope.$broadcast('scroll.refreshComplete');
@@ -87,6 +129,25 @@ angular.module('starter.controllers', [])
   }
 })
 
+.controller('SettingsCtrl', function($scope, $state, $http, Sessao) {
+  $scope.items = [
+  '../img/art1.jpeg',
+  '../img/art2.jpg',
+  '../img/art3.jpeg',
+  '../img/art4.jpg',
+  '../img/art5.jpg',
+  '../img/art6.jpg',
+  '../img/art7.jpg',
+  '../img/banner1.jpeg',
+  '../img/flower.jpg',
+  '../img/perry.png'
+  ]
+  $scope.newpost = function(){
+   $state.go('tab.registerp2');
+ }
+})
+
+
 .controller('Registerp1Ctrl', function($scope, $state, $http, Sessao) {
     $scope.data = {};
 
@@ -111,9 +172,9 @@ angular.module('starter.controllers', [])
 
 .controller('ProfileCtrl', function($scope, $state, $stateParams, $http, Sessao) {
  var usuario = Sessao.obter();
- $scope.item = function(){
-   $state.go("tab.item");
- }
+ $scope.goItem = function(item){
+   $state.go('tab.item', {artName: item.artName});
+}
 
  $scope.newpost = function(){
   $state.go('tab.registerp2');
@@ -129,13 +190,12 @@ angular.module('starter.controllers', [])
 })
 
 .controller('ItemCtrl', function($scope, $state, $stateParams, $http, Sessao) {
-  $scope.usuario = Sessao.obter();
 
-  $http.get('http://104.131.166.166:3000/profile/'+ $scope.usuario.username).then(function(resposta){
-    $scope.data = resposta.data[0];
-  });
+ $http.get('http://104.131.166.166:3000/item/'+ $stateParams.artName).then(function(resposta){
+   $scope.item = resposta.data[0];
+ });
 
-  $scope.backButton = function(){
-    $state.go("tab.profile");
-  }
+ $scope.backButton = function(){
+   $state.go("tab.profile");
+ }
 })
