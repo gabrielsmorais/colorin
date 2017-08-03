f02b43614e9276f681f972a68b473f6b648f527a
Gabriel Morais <gmorais1994@gmail.com>
2017-08-02 23:01:52 -0300

    versaoGhostbusters

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,24 @@
 angular.module('starter.controllers', [])
 
+.directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                        scope.fileread = loadEvent.target.result;
+                    });
+                }
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
+        }
+    }
+}])
+
 .directive('back', ['$window', function($window) {
         return {
             restrict: 'A',
